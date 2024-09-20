04195f4150d09162c5401857e9ad0d54eb48016c
prabhatdubey26 <prabhatdubey994@gmail.com>
2024-09-21 01:34:08 +0530

    feature: fronend steup and admin setup

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Samtech Admin | Login </title>
+    <title>ReviewBazaar Admin | Login </title>
     <!-- inject:css -->
     <link rel="stylesheet" href="{{ asset("admin/assets/css/style.css")}}" >
     <!-- endinject -->
--- resources/views/admin/partials/layout.blade.php
+++ resources/views/admin/partials/layout.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Samtech Admin</title>
+    <title>ReviewBazaar Admin</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{ asset("admin/assets/vendors/feather/feather.css")}}">
     <link rel="stylesheet" href="{{ asset("admin/assets/vendors/mdi/css/materialdesignicons.min.css")}}">
