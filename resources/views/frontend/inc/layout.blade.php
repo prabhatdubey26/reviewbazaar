72fba524b3154158eb376c5ff31e2273ce3e5abc
prabhatdubey26 <prabhatdubey994@gmail.com>
2024-10-10 23:14:50 +0530

    improve:update the blog meta tag

--- resources/views/frontend/inc/layout.blade.php
+++ resources/views/frontend/inc/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @yield('meta')
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.png')}}">
     <title>Review Bazzar</title>
     <link rel="stylesheet" href="{{ asset('css/app.min.css')}}">
